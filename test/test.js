984c61a8dff0e141e938260853569307dfb2e4ba
Cameron Koegel <ckoegel@bandwidth.com>
2021-10-21 10:04:04 -0400

    bad test

--- test/test.js
+++ test/test.js
@@ -9,29 +9,69 @@ const BW_VOICE_APPLICATION_ID = process.env.BW_VOICE_APPLICATION_ID;
 const BW_NUMBER = process.env.BW_NUMBER;
 const USER_NUMBER = process.env.USER_NUMBER;
 
+const DATA = "_data"
+
 
 var apiClient = NodeSdk.ApiClient.instance;
+//apiClient.returnType = NodeSdk.BandwidthMessage;
 var httpBasic = apiClient.authentications['httpBasic'];
 httpBasic.username = BW_USERNAME;
 httpBasic.password = BW_PASSWORD;
+console.log("baseapi return type: ", typeof apiClient.returnType);
 
-//-------------Messaging Tests-------------
-const messageApi = new  NodeSdk.MessagesApi();
-describe("Messaging Tests", function() {
-    it("Create SMS Message", function() {
-        let message_text = "node sdk test SMS";
-        let body = {
-            applicationId: BW_MESSAGING_APPLICATION_ID,
-            to: [USER_NUMBER],
-            from: BW_NUMBER,
-            text: message_text
-        }
-        let response = messageApi.createMessage(BW_ACCOUNT_ID, body);
-        console.log(response);
-    });
+var response;
+async function testdeeznuts() {
+    const messageApi = new  NodeSdk.MessagesApi();
+    let message_text = "node sdk test SMS";
+    let body = new NodeSdk.MessageRequest(
+        BW_MESSAGING_APPLICATION_ID, 
+        [USER_NUMBER], 
+        BW_NUMBER
+    );
+    //messageApi.returnType = NodeSdk.BandwidthMessage;
+    console.log("messages api return type: ", typeof messageApi.returnType)
+    body.text = message_text;
+    //let test = new NodeSdk.BandwidthMessage();
+    response = messageApi.createMessage(BW_ACCOUNT_ID, body, {}, function(){});
+    //let test1 = NodeSdk.BandwidthMessage.constructFromObject(response);
+    //response = messageApi.createMessage(BW_ACCOUNT_ID, test1);
+    return response
+}
+(async() => {
+    await testdeeznuts();
+    console.log("type:", typeof response)
+    console.log("is it a bwmessage?", response instanceof NodeSdk.BandwidthMessage);
+    console.log("response: ", response)
+})();
+console.log("type:", typeof response)
+console.log("#2", response instanceof NodeSdk.BandwidthMessage);
+//console.log("response: ", response)
+
+
+
+
+//-------------Messaging and Media Tests-------------
+// const messageApi = new  NodeSdk.MessagesApi();
+// describe("Messaging Tests", function() {
+//     it("Create SMS Message", function() {
+//         let message_text = "node sdk test SMS";
+//         let body = new NodeSdk.MessageRequest(
+//             BW_MESSAGING_APPLICATION_ID, 
+//             [USER_NUMBER], 
+//             BW_NUMBER
+//         );
+//         //messageApi.returnType = NodeSdk.BandwidthMessage;
+//         console.log("messages api return type: ", typeof messageApi.returnType)
+//         body.text = message_text;
+//         let test = new NodeSdk.BandwidthMessage();
+        
+//         let response = messageApi.createMessage(BW_ACCOUNT_ID, body, {}, function(){});
+
+//         let test1 = NodeSdk.BandwidthMessage.constructFromObject(response);
+//         //response = messageApi.createMessage(BW_ACCOUNT_ID, test1);
+//         console.log(typeof response)
+//         console.log(response instanceof NodeSdk.BandwidthMessage, test instanceof NodeSdk.BandwidthMessage, test1 instanceof NodeSdk.BandwidthMessage);
+//         console.log(response[DATA])
+//     });
     
-});
-
-//console.log(response);
-
-//console.log(apiClient.authentications);
+// });
